cd3690ff319a66f1813694c24ed32cf45089a3ae
Linus Jarbo <linus@linusjarbo.com>
2020-12-26 14:24:40 +0100

    Added digitboxes with material and to a list

---
+++ PiParty/Assets/Scripts/Main.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+
+    public DigitBoxHandler digitBoxHandler;
+
+    /*
+
+    What do I need to do?
+
+    add a very long list of PI and e numbers in separate files
+    add digitboxes/prefabs in a list.
+    add a tool that handles the positioning and spacing of digit boxes
+    add a button for each test criterion
+    a dropdown list for if statements?
+    add a camera so a web user can look at the structure
+
+    */
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
