8824f7a270f76d247a0c14691edb4ad4af815e6d
Linus Jarbo <linus@linusjarbo.com>
2020-12-27 22:28:34 +0100

    Added Phi constant
    Added flybycamera, build crashed.
    Scaled prefab

--- PiParty/Assets/Scripts/DigitBoxHandler.cs
+++ PiParty/Assets/Scripts/DigitBoxHandler.cs
@@ -2,20 +2,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DigitBoxHandler : MonoBehaviour
-{
-    public List<GameObject> digitBoxes = new List<GameObject>();
+public class DigitBoxHandler : MonoBehaviour {
 
+    public List<GameObject> digitBoxes = new List<GameObject> ();
+    private List<GameObject> addedDigitBoxes = new List<GameObject> ();
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    public int direction = 1;
 
+    private int gridSize = 25;
+
+    private int startPosx = 440;
+    private int startPosy = 500;
+
+    public int atCurrX = 0;
+    public int atCurrY = 0;
+
+    void Start () {
+        atCurrX = startPosx;
+        atCurrY = startPosy;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    private void CleanDigitBoxes () {
+        for (int i = 0; i < addedDigitBoxes.Count; i++) {
+            Destroy (addedDigitBoxes[i]);
+        }
+        addedDigitBoxes.Clear ();
+    }
 
+    public void changeDirection () {
+        direction *= -1;
+    }
+
+    public void oneRowDown (int digit) {
+        atCurrY -= gridSize;
+        changeDirection ();
+        //don't move in x dir
+        addNextNumber (digit, false);
+    }
+
+    public void addNextNumber (int digit, bool add = true) {
+
+        if (add) {
+            atCurrX -= direction * gridSize;
+        }
+        //instantiate
+        GameObject added = Instantiate (digitBoxes[digit], new Vector3 (atCurrX, atCurrY, 0), Quaternion.identity);
+        //add to list
+        addedDigitBoxes.Add (added);
     }
 }
--- PiParty/Assets/Scripts/Main.cs
+++ PiParty/Assets/Scripts/Main.cs
@@ -2,17 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Main : MonoBehaviour
-{
+public class Main : MonoBehaviour {
+    private int maxTesting = 10000;
 
     public DigitBoxHandler digitBoxHandler;
+    public Constants constants;
 
     /*
 
     What do I need to do?
 
-    add a very long list of PI and e numbers in separate files
-    add digitboxes/prefabs in a list.
+    Add sqrt2 and numbers that are known not to be clean
+    move all digit boxes to a parent
+    make a remove function for all digitboxes
+
     add a tool that handles the positioning and spacing of digit boxes
     add a button for each test criterion
     a dropdown list for if statements?
@@ -21,14 +24,27 @@ public class Main : MonoBehaviour
     */
 
     // Start is called before the first frame update
-    void Start()
-    {
-
+    void Start () {
+        MainFunction ();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    public void MainFunction () {
 
+        for (int i = 1; i < maxTesting; i++) {
+            //int digit = constants.PhiDigits[i];
+            //int digit = constants.PiDigits[i];
+            int digit = constants.EDigits[i];
+            //make a test criterion
+            if (digit == 3) {
+                //move down one row
+                    //change direction
+                digitBoxHandler.oneRowDown(digit);
+
+            }else{
+                digitBoxHandler.addNextNumber(digit);
+            }
+
+            //if number is 3, go down one row and change direction
+        }
     }
 }
